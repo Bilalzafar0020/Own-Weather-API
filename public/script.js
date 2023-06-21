0fc15fd152705fe2a5ec572a9b8e1bc511fc4f22
Bilalzafar0020 <bilalafridi0020@gmail.com>
2023-06-21 15:49:21 +0500

    Working on it

---
+++ public/script.js
@@ -0,0 +1,11 @@
+// curl ascii.live/forrest 
+
+// //  for the size of search bar 
+// let allContent = document.getElementById('search').addEventListener('click',()=>{
+//     let size = document.getElementById('container');
+// size.style.height = '37rem'
+
+// })
+
+
+
