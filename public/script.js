064a8d0b393153583301bb43308985417cd66f30
Bilalzafar0020 <bilalafridi0020@gmail.com>
2023-06-23 11:58:08 +0500

    Api created by using Express.js

--- public/script.js
+++ public/script.js
@@ -1,27 +1,27 @@
 // curl ascii.live/forrest 
 
-//  for the size of search bar 
-let size = document.getElementById('container');
-let input = document.getElementById('userInput');
+// //  for the size of search bar 
+// let size = document.getElementById('container');
+// let input = document.getElementById('userInput');
 
 
-let allContent = document.getElementById('search').addEventListener('click',()=>{
+// let allContent = document.getElementById('search').addEventListener('click',()=>{
 
-    let inputValue = input.value;  // so that we get the up to date value when the button is clicked not when the page is loaded
+//     let inputValue = input.value;  // so that we get the up to date value when the button is clicked not when the page is loaded
 
-if(inputValue === ''){
-    size.style.height = '7rem';
-    Swal.fire({
-        title: 'Please Enter City Name!',
-        icon: 'info',
-        confirmButtonText: 'OK'
-    });
-}
-else if(inputValue !== '') {
-size.style.height = '37rem';
-}
+// if(inputValue === ''){
+//     size.style.height = '7rem';
+//     Swal.fire({
+//         title: 'Please Enter City Name!',
+//         icon: 'info',
+//         confirmButtonText: 'OK'
+//     });
+// }
+// else if(inputValue !== '') {
+// size.style.height = '37rem';
+// }
 
-});
+// });
 
 
 
@@ -54,16 +54,47 @@ document.getElementById('pressure-label').innerText = data.pressurelabel;
              windSpeedImage.style.display='block';
              pressureImage.style.display='block';
 
-document.getElementById('max-label').style.display='block';
-document.getElementById('min-label').style.display='block';
-
-            } else {
-                Swal.fire('Error', `Weather data is not available for ${cityName}`, 'error');
-            }
+const size = document.getElementById('container');
+size.style.height = '37rem';
+            } 
         })
-        // .catch(error => {
-        //     Swal.fire('Error', `This city or place data is not available: ${cityName}`, 'error');
-        // });
-});
+
+        .catch(error => {
+              Swal.fire({
+        // title: 'This City or Place Data is not available',
+        text : ' This Weather data is not available. Please try again with a valid city name.',
+        icon: 'info',
+        confirmButtonText: 'OK'
+    });
+            const size = document.getElementById('container');
+            size.style.height = '7rem';
+               
+          
+// Clearing previous data because if the user first time enter the correct data but 2 time wrong so the previous data should be remove 
+            document.getElementById('main-temperature').textContent = '';
+            document.getElementById('max').textContent = '';
+            document.getElementById('min').textContent = '';
+            document.getElementById('city').textContent = '';
+            document.getElementById('country').textContent = '';
+            document.getElementById('how-many-humdity').textContent = '';
+            document.getElementById('how-many-windspeed').textContent = '';
+            document.getElementById('how-many-pressure').textContent = '';
+            document.getElementById('howmanyhumdity').innerText = '';
+            document.getElementById('wind-label').innerText = '';
+            document.getElementById('pressure-label').innerText = '';
+
+            const weatherImage = document.getElementById('weather-images');
+            const humidityImage = document.getElementById('humidity-image');
+            const windSpeedImage = document.getElementById('wind-speed-image');
+            const pressureImage = document.getElementById('pressure-image');
+
+            weatherImage.style.display = 'none';
+            humidityImage.style.display = 'none';
+            windSpeedImage.style.display = 'none';
+            pressureImage.style.display = 'none';
+        })
+            
+        });
+
 
 
